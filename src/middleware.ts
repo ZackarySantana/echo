86ce1ac624668ff8ab4b0fa66a089fed9cf5fd5e
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2025-10-27 21:38:29 -0400

    refactor: never redirect for noauth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,6 @@
 import { defineMiddleware } from "astro/middleware";
 import { stackFrom } from "./lib/stack-server";
 
-const noAuth = ["/", "/login", "/sign-up", "/oauth/callback"];
-
 export const onRequest = defineMiddleware(async (context, next) => {
     const timeBefore = Date.now();
     // const user = await stackFrom(context.request).getUser();
@@ -13,18 +11,6 @@ export const onRequest = defineMiddleware(async (context, next) => {
     if (user) {
         context.locals.user = user;
         context.locals.publicPageUser = user;
-    }
-
-    if (noAuth.includes(context.url.pathname)) {
-        console.log("Skip requiring auth for", context.url.pathname);
-        return next();
-    }
-
-    if (!user) {
-        return context.redirect(
-            "/login?next=" + encodeURIComponent(context.url.pathname),
-            302,
-        );
     }
 
     return next();
